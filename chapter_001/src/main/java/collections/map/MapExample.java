61980efe8cfa97f3fd95999921da3e9a300c438b
KirillAsmanov <mazusik1@gmail.com>
2020-05-08 15:46:00 +0300

    3. Переопределить только hashCode [#281952]

--- chapter_001/src/main/java/collections/map/MapExample.java
+++ chapter_001/src/main/java/collections/map/MapExample.java
@@ -7,19 +7,17 @@ import java.util.Map;
 
 public class MapExample {
     public static void main(String[] args) {
-        Map<User, String> userMap = new HashMap();
+        Map<User, String> userMap = new HashMap<>();
         User first = new User("Кирилл", 1, new GregorianCalendar(1997, Calendar.SEPTEMBER, 5));
         User second = new User("Кирилл", 1, new GregorianCalendar(1997, Calendar.SEPTEMBER, 5));
-        User third = second;
         userMap.put(first, "value1");
         userMap.put(second, "value2");
-        userMap.put(third, "REPLACEMENT");
+        System.out.println(first.equals(second)); // false
+        System.out.println(first.hashCode() == second.hashCode()); // true
         System.out.println(userMap);
-        // Оба объекта User являются уникальными потому, что они, будучи ссылочными типами, хранят ссылки на
-        // разные ячейки в памяти.
-        // Карта воспринимает их как два разных объекта, потому что по умолчанию метод equals в Object сравнивает
-        // адреса ссылок, которые не равны у этих объектов, и hashcode будет отличаться.
-        // Для того, чтобы карта могла опознать объекты как равные, нужно либо присвоить объекту такую же ссылку (third),
-        // либо переопределить у объекта методы equals() И hashcode().
+        // Здесь получилось, что хеш коды у двух объектов разные, но сами объекты не считаются равными по методу equals.
+        // В таком случае они добавятся в карту в один и тот же bucket (произойдет коллизия). Но значения не
+        // будут перезаписаны, а будут хранится в этом bucket'е в виде связанного списка, что, при большом количестве
+        // таковых сведет на нет преимущества карты в производительности
     }
 }
--- chapter_001/src/main/java/collections/map/User.java
+++ chapter_001/src/main/java/collections/map/User.java
@@ -32,7 +32,21 @@ public class User {
     public Calendar getBirthday() {
         return birthday;
     }
-
+/*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
+    }
+*/
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
 /* @Override
     public String toString() {
         return "User{" + "name='" + name + '\'' + ", children=" + children + '}';
