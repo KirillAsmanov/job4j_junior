32c579218d8036030c2bbca5bb0c16e467dbb086
KirillAsmanov <mazusik1@gmail.com>
2020-05-17 15:04:09 +0300

    4.0. File [#281977]

---
+++ chapter_002/src/main/java/inputoutput/Directory.java
@@ -0,0 +1,33 @@
+package inputoutput;
+
+import java.io.File;
+
+public class Directory {
+    /**
+     * Displays the name and size of ONLY the files in the directory and all subfolders
+     * @param file - path to directory
+     */
+    public void getAllFiles(File file) {
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+
+        for (File subfile : file.listFiles()) {
+            if (subfile.isDirectory()) {
+                getAllFiles(subfile);
+            } else if (subfile.isFile()) {
+                System.out.println("File name: " + subfile.getName() + " - size: " + subfile.length() + " bytes");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File("c:\\projects\\job4j_junior");
+        Directory dir = new Directory();
+        dir.getAllFiles(file);
+
+    }
+}
