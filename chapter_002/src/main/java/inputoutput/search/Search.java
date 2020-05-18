8bee192975fb67223f9b8a949a4cb86b7326075f
KirillAsmanov <mazusik1@gmail.com>
2020-05-18 18:33:13 +0300

    5. Валидация параметров запуска. [#281973]

--- chapter_002/src/main/java/inputoutput/search/Search.java
+++ chapter_002/src/main/java/inputoutput/search/Search.java
@@ -18,8 +18,11 @@ import static java.nio.file.FileVisitResult.*;
  */
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:\\projects\\job4j_junior");
-        search(start, "txt").forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     /**
