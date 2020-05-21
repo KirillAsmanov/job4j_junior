c26cacb1cdee30fe14d8925c883231d6d9bc595e
KirillAsmanov <mazusik1@gmail.com>
2020-05-21 16:04:07 +0300

    1. Бот [#281989]

--- chapter_002/src/main/java/socket/server/EchoServer.java
+++ chapter_002/src/main/java/socket/server/EchoServer.java
@@ -1,37 +1,92 @@
 package socket.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
+/**
+ * 0. Что такое Socket? [#281988]
+ * @since 20.05.2020
+ * @author Kirill Asmanov
+ */
 public class EchoServer {
     /**
-     * 0. Что такое Socket? [#281988]
-     * @since 20.05.2020
-     * @author Kirill Asmanov
+     * Handle the input from client
+     * @param in input stream
+     * @return input as a string
+     * @throws IOException
      */
+    public String inputHandler(BufferedReader in) throws IOException {
+        StringJoiner result = new StringJoiner(System.lineSeparator());
+        while (in.ready()) {
+            result.add(in.readLine());
+        }
+        return result.toString();
+    }
+
+    /**
+     * Handle output from server
+     * @param response response from client
+     * @param out output stream
+     * @throws IOException
+     */
+    public void outputHandler(String response, OutputStream out) throws IOException {
+        if (response == null) {
+            out.write("HTTP/1.1 400 ERROR\r\n\r\n".getBytes());
+            out.write("Cannot handle empty response".getBytes());
+            return;
+        }
+        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        out.write(getMessage(response).getBytes());
+    }
+
+    /**
+     * Checks is the server should be turned off
+     * @param response response from client
+     * @return true if it's should
+     */
+    public boolean checkSwitchCommand(String response) {
+        return !getMessage(response).equalsIgnoreCase("EXIT");
+    }
+
+    /**
+     * Extract message from client response
+     * @param response response from client
+     * @return message
+     */
+    private String getMessage(String response) {
+        return Arrays.stream(response.split(System.lineSeparator()))
+                .filter(s -> s.contains("GET"))
+                .map(s -> s.split("\\s"))
+                .flatMap(Arrays::stream)
+                .filter(s -> s.contains("/?msg="))
+                .map(s -> s.replace("/?msg=", ""))
+                .findFirst().orElse("EMPTY_RESPONSE");
+    }
+
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
+        EchoServer echo = new EchoServer();
+        try (ServerSocket server = new ServerSocket(8080)) {
+            System.out.println("Server started!");
             boolean active = true;
             while (active) {
                 Socket socket = server.accept();
+                System.out.println("Client connected!");
                 try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    String str = "-";
-                    while (!(str).isEmpty()) {
-                        str = in.readLine();
-                        System.out.println(str);
-                        if (str.startsWith("GET") && str.contains("?msg=Bye")) {
-                            active = false;
-                        }
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    String inputResponse = echo.inputHandler(in);
+                    System.out.println(inputResponse);
+
+                    System.out.println("Get message:");
+                    System.out.println(echo.getMessage(inputResponse) + System.lineSeparator());
+
+                    echo.outputHandler(inputResponse, out);
+                    active = echo.checkSwitchCommand(inputResponse);
                 }
             }
+            System.out.println("Server stopped!");
         }
     }
 }
