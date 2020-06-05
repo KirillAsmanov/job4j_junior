5b769b4c5845fe2eeee7d54c04afb1a85c6d8b66
KirillAsmanov <mazusik1@gmail.com>
2020-06-05 13:24:34 +0300

    3. Загрузка базы спамеров. [#281891]

---
+++ chapter_003/src/main/java/spamers/ImportDB.java
@@ -0,0 +1,69 @@
+package spamers;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines().forEach(s -> {
+                String[] splited = s.split(";");
+                users.add(new User(splited[0], splited[1]));
+            });
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("INSERT INTO spamers(name, email) VALUES (?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (InputStream in = ImportDB.class.getClassLoader().getResourceAsStream("spamers.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./dump.txt");
+        db.save(db.load());
+    }
+}
