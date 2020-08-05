fb3b3ffa4d9b84d7ea96cfdf3e36581ab7160a48
KirillAsmanov <mazusik1@gmail.com>
2020-08-05 14:37:28 +0300

    Демонстрация работы GC [#282018]

---
+++ chapter_004/src/main/java/GCtest.java
@@ -0,0 +1,57 @@
+import static com.carrotsearch.sizeof.RamUsageEstimator.sizeOf;
+
+public class GCtest {
+    static class User {
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("Я удалился!!!");
+        }
+
+        public User() {
+            System.out.println("Я родился ");
+        }
+
+    }
+
+    static class User2 {
+        long money;
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("Я удалился");
+        }
+
+        public User2() {
+            System.out.println("Я родился c полями" + System.lineSeparator());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Для выяснения размера объектов:
+        /* System.out.println("ДО СОЗДАНИЯ ОБЪЕКТОВ" + System.lineSeparator());
+        info();
+        System.out.println("СОЗДАЕМ 2 ОБЪЕКТА" + System.lineSeparator());
+        User user = new User();
+        User2 user2 = new User2();
+        System.out.println("Без полей: " + sizeOf(user));
+        System.out.println("С полем long: " + sizeOf(user2));
+        info(); */
+
+        // Для активации GC:
+        for (int i = 0; i <= 4000; i++) {
+            new User();
+            System.out.print(i + " ");
+        }
+    }
+ 
+    private static void info() {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("Used memory " + (runtime.totalMemory() - runtime.freeMemory()));
+        System.out.println("Free memory " + runtime.freeMemory());
+        System.out.println("Total memory " + runtime.totalMemory());
+        System.out.println("Max memory " + runtime.maxMemory());
+        System.out.print(System.lineSeparator());
+    }
+}
